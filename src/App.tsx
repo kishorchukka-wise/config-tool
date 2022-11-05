c8ca4f8d24b8a04c6632e177640d28a07fe2ae74
Kishor Chukka <kishor.chukka@globalsign.com>
2022-11-05 15:58:55 +0000

    added logic to handle dropdowns

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Layout, Col, Row, Select, Checkbox, Form, Input } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Layout, Col, Row, Select, Checkbox, Form, Input, Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,9 +15,21 @@ const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
+type ConfigItem = {
+  name: string;
+  values: string[];
+}
+
+type Config = {
+  partnerId: string;
+  configItems: ConfigItem[];
+}
+
 function App() {
+  const initialConfig: Config = { partnerId: "abx", configItems: [] };
   const [form] = Form.useForm();
-  const [data, setData] = useState("other");
+  const [config, setConfig] = useState<Config>(initialConfig);
+
   const onFinish = (values: any) => {
     console.log(values);
   };
@@ -26,16 +38,55 @@ function App() {
     form.resetFields();
   };
 
-  const onFill = () => {
-    form.setFieldsValue({
-      note: 'Hello world!',
-      gender: 'male',
-    });
-  };
+  const findConfigItem = (name: string): number => {
+    return config.configItems.findIndex(item => item.name == name);
+  }
+
   //var data = { gender: "" };
-  const onGenderChange = (value: string) => {
-    setData(value)
+  const onCustomerModelChange = (value: string) => {
+    const idx = findConfigItem('customerModel');
+    if (idx != -1) {
+      config.configItems[idx].values = [value];
+    } else {
+      const configItem: ConfigItem = { name: '', values: [] };
+      configItem.name = 'customerModel';
+      configItem.values.push(value);
+      config.configItems.push(configItem);
+    }
+    setConfig({ ...config });
   };
+
+  const onInterfaceModelChange = (value: string) => {
+    const idx = findConfigItem('interfaceModel');
+    if (idx != -1) {
+      config.configItems[idx].values = [value];
+    } else {
+      const configItem: ConfigItem = { name: '', values: [] };
+      configItem.name = 'interfaceModel';
+      configItem.values.push(value);
+      config.configItems.push(configItem);
+    }
+    setConfig({ ...config });
+  };
+
+  const onLicenseModelChange = (value: string) => {
+    const idx = findConfigItem('licenseModel');
+    if (idx != -1) {
+      config.configItems[idx].values = [value];
+    } else {
+      const configItem: ConfigItem = { name: '', values: [] };
+      configItem.name = 'licenseModel';
+      configItem.values.push(value);
+      config.configItems.push(configItem);
+    }
+    setConfig({ ...config });
+  };
+
+  const onPartnerIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value: inputValue } = e.target;
+    config.partnerId = inputValue;
+    setConfig({ ...config });
+  }
 
   return (
     <Layout>
@@ -45,10 +96,16 @@ function App() {
           <Col span={12}>
             <h2>Checklist</h2>
             <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-              <Form.Item name="gender" label="Customer Model" rules={[{ required: true }]}>
+              <Form.Item name="partnerId" label="Partner Id" rules={[{ required: true }]}>
+                <Input value={config.partnerId} onChange={onPartnerIdChange} />
+              </Form.Item>
+              <Form.Item name="partnerName" label="Partner Name" rules={[{ required: true }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item name="customerModel" label="Customer Model" rules={[{ required: true }]}>
                 <Select
-                  placeholder="Select a option and change input text above"
-                  onChange={onGenderChange}
+                  placeholder="Select a option"
+                  onChange={onCustomerModelChange}
                   allowClear
                 >
                   <Option value="male">male</Option>
@@ -56,11 +113,40 @@ function App() {
                   <Option value="other">other</Option>
                 </Select>
               </Form.Item>
+              <Form.Item name="interfaceModel" label="Interface Model" rules={[{ required: true }]}>
+                <Select
+                  placeholder="Select a option"
+                  onChange={onInterfaceModelChange}
+                  allowClear
+                >
+                  <Option value="male">male</Option>
+                  <Option value="female">female</Option>
+                  <Option value="other">other</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item name="licenseModel" label="License Model" rules={[{ required: true }]}>
+                <Select
+                  placeholder="Select a option"
+                  onChange={onLicenseModelChange}
+                  allowClear
+                >
+                  <Option value="male">male</Option>
+                  <Option value="female">female</Option>
+                  <Option value="other">other</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item {...tailLayout}>
+                <Button htmlType="button" onClick={onReset}>
+                  Reset
+                </Button>
+              </Form.Item>
             </Form>
           </Col>
           <Col span={12}>
-            <h2>Configuration</h2>
-            <TextArea rows={4} value={JSON.stringify(data, null, 2)} contentEditable={false} />
+            <h2>Partner Configuration</h2>
+            <TextArea rows={4} value={JSON.stringify(config, null, 2)} contentEditable={false} />
+            <h2>Edge Auth Policies</h2>
+            <TextArea rows={4} value={JSON.stringify(config, null, 2)} contentEditable={false} />
           </Col>
         </Row>
       </Content>
